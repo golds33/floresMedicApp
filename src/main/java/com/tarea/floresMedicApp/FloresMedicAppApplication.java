8cdadcc25a29cecaf9774e3dba0e63c435b92c62
juanF <juanflores@gmail.com>
2025-06-23 18:21:46 -0500

    haciendo inicio tarea

---
+++ src/main/java/com/tarea/floresMedicApp/FloresMedicAppApplication.java
@@ -0,0 +1,13 @@
+package com.tarea.floresMedicApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloresMedicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloresMedicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea/floresMedicApp/FloresMedicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea.floresMedicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloresMedicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
